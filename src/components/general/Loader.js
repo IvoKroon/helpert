faa9413bef42077b7fec3196654bafd8ff8bbf5a
Ivo Kroon <>
2018-06-26 17:06:42 +0200

    loader update

--- src/components/general/Loader.js
+++ src/components/general/Loader.js
@@ -5,13 +5,13 @@ import LoadIcon from '../icons/LoadIcon';
 const Holder = styled.div`
   margin-left: auto;
   margin-right: auto;
-  margin-top: 80px;
+  margin-top: 40px;
   width: 40px;
 `;
 
 const Loader = () => (
   <Holder>
-    <LoadIcon width="100" height="100" color="#061456" />
+    <LoadIcon width="50" height="50" color="#061456" />
   </Holder>
 );
 
